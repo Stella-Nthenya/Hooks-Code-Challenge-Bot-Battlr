9c7b08a997a5b3113c1e38e73b0db17ad17e7869
Stella Nthenya <stellaalogi@gmail.com>
2023-06-04 23:43:58 +0300

    displayed all bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,13 +1,55 @@
-import React from "react";
-// ALL BOTS WILL BE DISPLAYED
+import React, { useState, useEffect } from "react";
+
 function BotCollection() {
-  // Your code here
+  const [allBotsData, setAllBotsData] = useState([]);
+
+  useEffect(() => {
+    const getAllBots = async (url) => {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          const data = await response.json();
+          setAllBotsData(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getAllBots("http://localhost:8002/bots");
+  }, []);
+
   return (
-    <div className="ui four column grid">
-      <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
-      </div>
+    <div className="all-bots-container">
+      {/* Collection of all bots */}
+      {allBotsData.map(
+        ({
+          id,
+          name,
+          health,
+          damage,
+          armor,
+          bot_class,
+          catchphrase,
+          avatar_url,
+        }) => (
+          <div className={`bot-container ${bot_class}`} key={id}>
+            <div className="img-div">
+              <img src={avatar_url} alt={name} width="100" height="100" />
+            </div>
+            <p className="bot-name">{name}</p>
+            <p>{catchphrase}</p>
+            <div className="bot-container-footer">
+              <div>{health}</div>
+              <div>{damage}</div>
+              <div>{armor}</div>
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 }
